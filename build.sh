9fb2b1ae4113c827338dfd79df92f71fbf65ed5a
rgps4502 <rgps4502>
2019-10-03 03:59:21 +0800

    update

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -ex
 USERNAME=129729052534.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
 IMAGE=dc103_repo_dev
-docker build -t $USERNAME/$IMAGE:latest
+docker build -t $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
